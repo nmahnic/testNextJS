2a9c669a6acebba32fe33205e2e5dee7a0a3469e
Nicolas Mahnic <nico.mahnic94@gmail.com>
2022-02-14 19:22:45 -0300

    add timeline page using Link

---
+++ pages/timeline/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next"
+import Link from "next/link"
+
+const Timeline: NextPage = () =>  {
+    return (
+        <div>
+            <h1> This is the timeline </h1>
+            <Link href="/">Go Home</Link>
+        </div>
+    )
+}
+
+export default Timeline
