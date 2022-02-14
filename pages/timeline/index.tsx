a36d9316e80809e5b50c9f976b8f1a2c4579dfd6
Nicolas Mahnic <nico.mahnic94@gmail.com>
2022-02-14 20:00:43 -0300

    add timeline

--- pages/timeline/index.tsx
+++ pages/timeline/index.tsx
@@ -1,13 +1,14 @@
 import { NextPage } from "next"
 import Link from "next/link"
+import AppLayout from "../../components/AppLayout"
 
 const Timeline: NextPage = () =>  {
-    return (
-        <div>
-            <h1> This is the timeline </h1>
-            <Link href="/">Go Home</Link>
-        </div>
-    )
+  return (
+    <AppLayout>
+      <h1> This is the timeline </h1>
+      <Link href="/">Go Home</Link>
+    </AppLayout>
+  )
 }
 
 export default Timeline
